b89f76b75e939746909161b5d74c3eb533ffa7ee
ReQ1964 <slomianykacper4@gmail.com>
2023-04-09 18:57:05 +0200

    [change] Carousel icons change and basic styling changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,19 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from './App'
+import HomePage from './pages/Home/HomePage';
 import './index.scss'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+    children: [
+      {
+        index: true,
+        element: <HomePage/>
+      }
+    ]
   },
 ]);
 
