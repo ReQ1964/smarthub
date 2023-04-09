70b91e3a6b1e7d8caba354d492ced89a60488b4a
ReQ1964 <slomianykacper4@gmail.com>
2023-04-09 17:34:41 +0200

    [change] Change name and add cart icon

--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -2,13 +2,15 @@ import classes from './Navbar.module.scss'
 import SearchIcon from '../../assets/icon/search.svg'
 import AccountIcon from '../../assets/icon/account.svg'
 import HamburgerIcon from '../../assets/icon/hamburger.svg'
+import CartIcon from '../../assets/icon/cart.svg'
 
 const Navbar = () => {
     return <nav>
         <div className={classes.heading}>
-            <h1>Gadget Grove</h1>
+            <h1>SmartHub</h1>
             <div className={classes.controls}>
                 <img src={SearchIcon} alt="" />
+                <img src={CartIcon} alt="" />
                 <img src={AccountIcon} alt="" />
                 <img src={HamburgerIcon} alt="" />
             </div>
