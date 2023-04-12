5369b7888946f900ab218e76b877817a9df83e32
ReQ1964 <slomianykacper4@gmail.com>
2023-04-12 21:35:15 +0200

    [feat] Add Bestseller component

---
+++ src/pages/Home/Bestseller/Bestseller.jsx
@@ -0,0 +1,55 @@
+import classes from './Bestseller.module.scss'
+import phone from '../../../assets/img/example-phone.png'
+import Product from '../../../components/UI/Product'
+import Button from '../../../components/UI/Button'
+
+const DUMMY_PRODUCTS = [
+    {
+        id: 'p1',
+        name: 'Phone',
+        company: 'Phone Company',
+        price: 150,
+        img: phone,
+        colors: ['orange', 'green', 'black'],
+    },
+    {
+        id: 'p2',
+        name: 'Phone2',
+        company: 'Phone Company',
+        price: 150,
+        img: phone,
+        colors: ['orange', 'green', 'black'],
+    },
+    {
+        id: 'p3',
+        name: 'Phone3',
+        company: 'Phone Company',
+        price: 150,
+        img: phone,
+        colors: ['orange', 'green', 'black'],
+    },
+    {
+        id: 'p4',
+        name: 'Phone4',
+        company: 'Phone Company',
+        price: 150,
+        img: phone,
+        colors: ['orange', 'green', 'black'],
+    }
+]
+
+const Bestseller = () => {
+    return <section className={classes.bestseller}>
+        <h3>Featured Products</h3>
+        <h1>Bestseller products</h1>
+        <ul className={classes.products}>
+        {DUMMY_PRODUCTS.map(product => {
+            return <Product id={product.id} img={product.img} name={product.name} price={product.price} company={product.company} colors={product.colors}/>
+        })}
+        </ul>
+        <Button className={classes.btn}>BROWSE MORE</Button>
+
+    </section>
+}
+
+export default Bestseller
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,10 +1,12 @@
 import Categories from "./Category/Categories"
 import Hero from "./Hero/Hero"
+import Bestseller from "./Bestseller/Bestseller"
 
 const HomePage = () => {
     return <main>
         <Hero/>
         <Categories/>
+        <Bestseller/>
     </main>
 }
 
