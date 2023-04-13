944a0477e34596f9e699cde9f06f7311fbe5f5fa
ReQ1964 <slomianykacper4@gmail.com>
2023-04-13 16:50:48 +0200

    [feat] Add Review component with dummy review

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,12 +1,14 @@
 import Categories from "./Category/Categories"
 import Hero from "./Hero/Hero"
 import Bestseller from "./Bestseller/Bestseller"
+import Review from "./Review/Review"
 
 const HomePage = () => {
     return <main>
         <Hero/>
         <Categories/>
         <Bestseller/>
+        <Review/>
     </main>
 }
 
