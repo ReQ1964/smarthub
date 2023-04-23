d906af1332f5c341bbab884643a5468ba52e382f
ReQ1964 <slomianykacper4@gmail.com>
2023-04-23 20:21:15 +0200

    [feat] Add and implement custom hook for scrolling to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router'
 import Navbar from './layouts/Navbar/Navbar'
 import Footer from './layouts/Footer/Footer'
+import useScrollToTop from './hooks/useScrollToTop'
 import './App.scss'
 
 function App() {
+  useScrollToTop()
+  
   return (
     <div className="App">
       <Navbar/>
