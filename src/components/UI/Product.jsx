901197d0b5634d749ef78e12d9353a002e291551
ReQ1964 <slomianykacper4@gmail.com>
2023-05-02 19:58:24 +0200

    [update] Change styling, spacing

--- src/components/UI/Product.jsx
+++ src/components/UI/Product.jsx
@@ -2,7 +2,7 @@ import classes from './Product.module.scss'
 
 const Product = (props) => {
     return <li onClick={props.onClick} className={classes.product}>
-                <img src={props.img} alt="" />
+                <img src={Object.values(props.img)[0]} alt="" />
                 <div className={classes.description}>
                     <h4>{props.name}</h4>
                     <p className={classes.company}>{props.company}</p>
--- src/components/UI/SimilarProduct.jsx
+++ src/components/UI/SimilarProduct.jsx
@@ -3,7 +3,7 @@ import classes from './SimilarProduct.module.scss';
 const SimilarProduct = (props) => {
 	return (
 		<div className={classes.product} onClick={props.onClick}>
-            <img src={props.img} alt="" />
+            <img src={Object.values(props.img)[0]} alt="" />
 			<h4>{props.name}</h4>
 			<p className={classes.company}>{props.company}</p>
 			<p className={classes.price}>${props.price}</p>
