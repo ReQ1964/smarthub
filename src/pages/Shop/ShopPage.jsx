ef32d3fb4a0a42adf0c7739050cde79619e78ab5
ReQ1964 <slomianykacper4@gmail.com>
2023-05-03 11:05:50 +0200

    [fix] Fix dummy photo import to ShopPage

--- src/pages/Shop/ShopPage.jsx
+++ src/pages/Shop/ShopPage.jsx
@@ -4,50 +4,19 @@ import Categories from "./Categories/Categories"
 import Filter from "./Filter/Filter"
 import Products from "./Products/Products"
 import Pagination from "./Pagination/Pagination"
-import phone from '../../assets/img/example-phone.png'
+import { useSelector } from "react-redux"
 
-export const DUMMY_PRODUCTS = [
-    {
-        id: 'p1',
-        name: 'Phone',
-        company: 'Phone Company',
-        price: 150,
-        img: phone,
-        colors: ['orange', 'green', 'black'],
-    },
-    {
-        id: 'p2',
-        name: 'Phone2',
-        company: 'Phone Company',
-        price: 150,
-        img: phone,
-        colors: ['orange', 'green', 'black'],
-    },
-    {
-        id: 'p3',
-        name: 'Phone3',
-        company: 'Phone Company',
-        price: 150,
-        img: phone,
-        colors: ['orange', 'green', 'black'],
-    },
-    {
-        id: 'p4',
-        name: 'Phone4',
-        company: 'Phone Company',
-        price: 150,
-        img: phone,
-        colors: ['orange', 'green', 'black'],
-    }
-]
 
 const ShopPage = () => {
+	const data = useSelector((state) => state.products.products);
+    const products = Object.values(data)
+
     const [currentPage, setCurrentPage] = useState(1)
     const [productsPerPage, setProductsPerPage] = useState(2)
 
     const indexOfLastProduct = currentPage * productsPerPage;
     const indexOfFirstProduct = indexOfLastProduct - productsPerPage
-    const currentProducts = DUMMY_PRODUCTS.slice(indexOfFirstProduct, indexOfLastProduct)
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct)
 
     const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
@@ -56,7 +25,7 @@ const ShopPage = () => {
         <Categories/>
         <Filter/>
         <Products products={currentProducts}/>
-        <Pagination productsPerPage={productsPerPage} totalProducts={DUMMY_PRODUCTS.length} paginate={paginate} currentPage={currentPage}/>
+        <Pagination productsPerPage={productsPerPage} totalProducts={products.length} paginate={paginate} currentPage={currentPage}/>
     </main>
 }
 
