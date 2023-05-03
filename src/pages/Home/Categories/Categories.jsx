ffad8e4367e1ffbccda7879d5b6b7cc8e955c5d1
ReQ1964 <slomianykacper4@gmail.com>
2023-05-03 18:08:57 +0200

    [feat] Add redirection from HomePage to ShopPage through categories

--- src/pages/Home/Categories/Categories.jsx
+++ src/pages/Home/Categories/Categories.jsx
@@ -1,25 +1,46 @@
-import classes from './Categories.module.scss'
-import smartwatch from '../../../assets/img/home-categories/smartwatch.png'
-import phone from '../../../assets/img/home-categories/phone.png'
-
+import classes from './Categories.module.scss';
+import smartwatch from '../../../assets/img/home-categories/smartwatch.png';
+import phone from '../../../assets/img/home-categories/phone.png';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setFilteredProducts } from '../../../store/products-slice';
 
 const Categories = () => {
-    return <section className={classes.categories}>
-        <div className={`${classes.category}`}>
-            <p className={classes.alert}>Ends Today</p>
-            <h2>Elements Of Style</h2>
-            <p>Top Ten Products of the Week</p>
-            <a href="">Explore Items</a>
-            <img src={smartwatch} alt="" />
-        </div>
-        <div className={`${classes.category}`}>
-            <p>Your Space</p>
-            <h2>Modern Life</h2>
-            <p>Top Ten Products of the Week</p>
-            <a href="">Explore Items</a>
-            <img src={[phone]} alt="" />
-        </div>
-    </section>
-}
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-export default Categories
+	return (
+		<section className={classes.categories}>
+			<div className={`${classes.category}`}>
+				<p className={classes.alert}>Ends Today</p>
+				<h2>Elements Of Style</h2>
+				<p>Top Ten Products of the Week</p>
+				<a
+					onClick={() => {
+						navigate('/shop');
+						dispatch(setFilteredProducts('watch'));
+					}}
+				>
+					Explore Items
+				</a>
+				<img src={smartwatch} alt="" />
+			</div>
+			<div className={`${classes.category}`}>
+				<p>Your Space</p>
+				<h2>Modern Life</h2>
+				<p>Top Ten Products of the Week</p>
+				<a
+					onClick={() => {
+						navigate('/shop');
+						dispatch(setFilteredProducts('phone'));
+					}}
+				>
+					Explore Items
+				</a>
+				<img src={[phone]} alt="" />
+			</div>
+		</section>
+	);
+};
+
+export default Categories;
