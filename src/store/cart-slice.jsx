2c87ae11134b149a9916aac6c54d370af3ce1949
ReQ1964 <slomianykacper4@gmail.com>
2023-05-07 14:22:01 +0200

    [feat] Add blank cart slice

---
+++ src/store/cart-slice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const cartSlice = createSlice({
+	name: 'products',
+	initialState: initialState,
+	reducers: {},
+});
+
+export const {} = cartSlice.actions;
+
+export default cartSlice.reducer;
