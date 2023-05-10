588702f9a5065dee132740c5faf842ca31a7ec3d
ReQ1964 <slomianykacper4@gmail.com>
2023-05-10 13:24:01 +0200

    [feat] Add OrderItems to Order Details

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -5,6 +5,7 @@ import Button from '../../components/UI/Button';
 import { CountryDropdown, RegionDropdown } from 'react-country-region-selector';
 import * as yup from 'yup';
 import { useNavigate } from 'react-router-dom';
+import OrderItems from './OrderItems/OrderItems';
 
 const Details = () => {
 	const navigate = useNavigate();
@@ -48,71 +49,74 @@ const Details = () => {
 	});
 
 	return (
-		<form
-			className={classes.details}
-			onSubmit={handleSubmit((data) => {
-				console.log(data);
-				navigate('/order/shipping');
-			})}
-		>
-			<h2>Contact</h2>
-			<input {...register('email')} placeholder="Email*" />
-			<p className={classes.error}>{errors.email?.message}</p>
+		<>
+			<OrderItems />
+			<form
+				className={classes.details}
+				onSubmit={handleSubmit((data) => {
+					console.log(data);
+					navigate('/order/shipping');
+				})}
+			>
+				<h2>Contact</h2>
+				<input {...register('email')} placeholder="Email*" />
+				<p className={classes.error}>{errors.email?.message}</p>
 
-			<input {...register('number')} placeholder="Phone Number*" />
-			<p className={classes.error}>{errors.number?.message}</p>
+				<input {...register('number')} placeholder="Phone Number*" />
+				<p className={classes.error}>{errors.number?.message}</p>
 
-			<h2>Shipping</h2>
-			<input {...register('name')} placeholder="Name*" />
-			<p className={classes.error}>{errors.name?.message}</p>
+				<h2>Shipping</h2>
+				<input {...register('name')} placeholder="Name*" />
+				<p className={classes.error}>{errors.name?.message}</p>
 
-			<input {...register('surname')} placeholder="Surname*" />
-			<p className={classes.error}>{errors.surname?.message}</p>
+				<input {...register('surname')} placeholder="Surname*" />
+				<p className={classes.error}>{errors.surname?.message}</p>
 
-			<input {...register('address')} placeholder="Address*" />
-			<p className={classes.error}>{errors.address?.message}</p>
+				<input {...register('address')} placeholder="Address*" />
+				<p className={classes.error}>{errors.address?.message}</p>
 
-			<input {...register('city')} placeholder="City*" />
-			<p className={classes.error}>{errors.city?.message}</p>
+				<input {...register('city')} placeholder="City*" />
+				<p className={classes.error}>{errors.city?.message}</p>
 
-			<input {...register('postal')} placeholder="Postal Code*" />
-			<p className={classes.error}>{errors.postal?.message}</p>
+				<input {...register('postal')} placeholder="Postal Code*" />
+				<p className={classes.error}>{errors.postal?.message}</p>
 
-			<input {...register('note')} placeholder="Shipping Note" />
-			<p className={classes.error}>{errors.note?.message}</p>
+				<input {...register('note')} placeholder="Shipping Note" />
+				<p className={classes.error}>{errors.note?.message}</p>
 
-			<Controller
-				name="country"
-				render={({ field: { name, value, onChange } }) => (
-					<CountryDropdown
-						name={name}
-						value={value}
-						onChange={onChange}
-						classes={classes.dropdown}
-					/>
-				)}
-				control={control}
-			/>
-			<p className={classes.error}>{errors.country?.message}</p>
-			<Controller
-				name="region"
-				render={({ field: { name, value, onChange } }) => (
-					<RegionDropdown
-						country={watch('country')}
-						name={name}
-						value={value}
-						onChange={onChange}
-						classes={classes.dropdown}
-					/>
-				)}
-				control={control}
-			/>
-			<p className={classes.error}>{errors.region?.message}</p>
+				<Controller
+					name="country"
+					render={({ field: { name, value, onChange } }) => (
+						<CountryDropdown
+							name={name}
+							value={value}
+							onChange={onChange}
+							classes={classes.dropdown}
+						/>
+					)}
+					control={control}
+				/>
+				<p className={classes.error}>{errors.country?.message}</p>
+				<Controller
+					name="region"
+					render={({ field: { name, value, onChange } }) => (
+						<RegionDropdown
+							country={watch('country')}
+							name={name}
+							value={value}
+							onChange={onChange}
+							classes={classes.dropdown}
+						/>
+					)}
+					control={control}
+				/>
+				<p className={classes.error}>{errors.region?.message}</p>
 
-			<Button type="submit" className={classes.btn}>
-				Go to shipping
-			</Button>
-		</form>
+				<Button type="submit" className={classes.btn}>
+					Go to shipping
+				</Button>
+			</form>
+		</>
 	);
 };
 
