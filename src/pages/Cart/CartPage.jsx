310145e126339e42a3ba62c1862c6002700b5508
ReQ1964 <slomianykacper4@gmail.com>
2023-05-13 12:01:13 +0200

    [feat] Add Modal to Cart to prevent from checkout without any items in cart

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -3,13 +3,30 @@ import Button from '../../components/UI/Button';
 import CartItems from './CartItems/CartItems';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Modal from '../../components/Modal/Modal';
 
 const CartPage = () => {
 	const navigate = useNavigate();
+	const [isOpen, setIsOpen] = useState(false);
+	const [modalMessage, setModalMessage] = useState('');
 	const totalPrice = useSelector((state) => state.cart.totalPrice);
+	const products = useSelector((state) => state.cart.products);
+
+	const checkoutHandler = () => {
+		if (products.length === 0) {
+			setModalMessage("Your cart can't be empty!");
+			setIsOpen(true);
+			return;
+		}
+		navigate('/order/details');
+	};
 
 	return (
 		<main className={classes.cart}>
+			<Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+				{modalMessage}
+			</Modal>
 			<h1>Your Cart Items</h1>
 			<div className={classes.info}>
 				<p>Product</p>
@@ -17,10 +34,7 @@ const CartPage = () => {
 			</div>
 			<CartItems />
 			<h3>Total price: ${totalPrice}</h3>
-			<Button
-				className={classes.checkout}
-				onClick={() => navigate('/order/details')}
-			>
+			<Button className={classes.checkout} onClick={checkoutHandler}>
 				Checkout
 			</Button>
 		</main>
