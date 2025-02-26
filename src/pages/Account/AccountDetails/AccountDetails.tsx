14f098f7636e2026130330656c13b56b0c88553e
ReQ1964 <slomianykacper4@gmail.com>
2025-02-26 18:43:38 +0100

    feat: update deprecated packages, revert firebase implementation for auth

--- src/pages/Account/AccountDetails/AccountDetails.tsx
+++ src/pages/Account/AccountDetails/AccountDetails.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
 import Button from '@/components/UI/Button/Button';
-import { signOut } from 'firebase/auth';
-import { auth } from '@/firebase';
 import classes from './AccountDetails.module.scss';
 
 const AccountDetails = () => {
-  const handleLogout = () => {
-    signOut(auth)
-      .then(() => {
-        console.log('Signed out successfully');
-      })
-      .catch((error) => {
-        console.log(error.code, error.message);
-      });
-  };
+  const handleLogout = () => {};
 
   return (
     <section className={classes.account}>
