55bf40e6b47c771cea7b0753d0f4f14c5fee00ec
ReQ1964 <slomianykacper4@gmail.com>
2025-03-10 19:25:09 +0100

    refactor: refactor cartStore

--- src/store/cartStore.tsx
+++ src/store/cartStore.tsx
@@ -11,7 +11,7 @@ interface ICartProduct {
   quantity: number;
 }
 
-interface CartState {
+interface ICartState {
   cartProducts: ICartProduct[];
   totalPrice: number;
   totalQuantity: number;
@@ -23,18 +23,22 @@ interface CartState {
   findProduct: (id: string | number) => ICartProduct | undefined;
 }
 
-const calculateTotalPrice = (products: ICartProduct[]) => {
-  return products.reduce(
+const calculateTotalPrice = (products: ICartProduct[]) =>
+  products.reduce(
     (total, product) => total + product.price * product.quantity,
     0,
   );
-};
 
-const calculateProductsQuantity = (products: ICartProduct[]) => {
-  return products.reduce((total, product) => total + product.quantity, 0);
-};
+const calculateProductsQuantity = (products: ICartProduct[]) =>
+  products.reduce((total, product) => total + product.quantity, 0);
 
-const useCartStore = create<CartState>((set, get) => ({
+const updateCartState = (products: ICartProduct[]) => ({
+  cartProducts: products,
+  totalPrice: calculateTotalPrice(products),
+  totalQuantity: calculateProductsQuantity(products),
+});
+
+const useCartStore = create<ICartState>((set, get) => ({
   cartProducts: [],
   totalPrice: 0,
   totalQuantity: 0,
@@ -43,37 +47,23 @@ const useCartStore = create<CartState>((set, get) => ({
       const existingProduct = state.cartProducts.find(
         (product) => product.id === newProduct.id,
       );
-      let updatedProducts: ICartProduct[] = [];
 
-      if (existingProduct) {
-        updatedProducts = state.cartProducts.map((product) =>
-          product.id === newProduct.id
-            ? { ...product, quantity: product.quantity + 1 }
-            : product,
-        );
-      } else {
-        updatedProducts = [
-          ...state.cartProducts,
-          { ...newProduct, quantity: 1 },
-        ];
-      }
-      return {
-        cartProducts: updatedProducts,
-        totalPrice: calculateTotalPrice(updatedProducts),
-        totalQuantity: calculateProductsQuantity(updatedProducts),
-      };
+      const updatedProducts = existingProduct
+        ? state.cartProducts.map((product) =>
+            product.id === newProduct.id
+              ? { ...product, quantity: product.quantity + 1 }
+              : product,
+          )
+        : [...state.cartProducts, { ...newProduct, quantity: 1 }];
+
+      return updateCartState(updatedProducts);
     }),
   removeFromCart: (id) =>
     set((state) => {
       const updatedProducts = state.cartProducts.filter(
         (product) => product.id !== id,
       );
-
-      return {
-        cartProducts: updatedProducts,
-        totalPrice: calculateTotalPrice(updatedProducts),
-        totalQuantity: calculateProductsQuantity(updatedProducts),
-      };
+      return updateCartState(updatedProducts);
     }),
   increaseQuantity: (id) =>
     set((state) => {
@@ -82,12 +72,7 @@ const useCartStore = create<CartState>((set, get) => ({
           ? { ...product, quantity: product.quantity + 1 }
           : product,
       );
-
-      return {
-        cartProducts: updatedProducts,
-        totalPrice: calculateTotalPrice(updatedProducts),
-        totalQuantity: calculateProductsQuantity(updatedProducts),
-      };
+      return updateCartState(updatedProducts);
     }),
   decreaseQuantity: (id) =>
     set((state) => {
@@ -96,22 +81,11 @@ const useCartStore = create<CartState>((set, get) => ({
           ? { ...product, quantity: Math.max(1, product.quantity - 1) }
           : product,
       );
-
-      return {
-        cartProducts: updatedProducts,
-        totalPrice: calculateTotalPrice(updatedProducts),
-        totalQuantity: calculateProductsQuantity(updatedProducts),
-      };
+      return updateCartState(updatedProducts);
     }),
-  clearCart: () =>
-    set({
-      cartProducts: [],
-      totalPrice: 0,
-      totalQuantity: 0,
-    }),
+  clearCart: () => set(() => updateCartState([])),
   findProduct: (id) => {
-    const state = get();
-    return state.cartProducts.find((product) => product.id === id);
+    return get().cartProducts.find((product) => product.id === id);
   },
 }));
 
